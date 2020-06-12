8ffe0eafb6c540a2b5098a66942fd4cfaec92eb7
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-12 09:22:53 -0700

    parse the user input and store it in roboCount

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $("form#formInput").submit(function(event) {
     event.preventDefault();
+    const numInput = parseInt($("input#numInput").val());
+    console.log(numInput);
 
   });
 }); 
