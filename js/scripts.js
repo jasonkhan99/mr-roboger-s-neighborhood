5816933d9f50089c9ff2c7cbda36e57a301e29d9
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-12 10:46:10 -0700

    change parseInt to Float, complete first spec with if statement.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,19 @@
-// Business Logic
-function roboResponse(numInput) {
-  return false;
+//Business Logic
+function roboResponse(roboCount) {
+  if (roboCount < 0 || roboCount % 1 !== 0) {
+    return "Please enter a Whole Number.";
+  } else {
+    return false;
+  }
 }
-
-
-
-
-
 
 // User Logic
 $(document).ready(function() {
   $("form#formInput").submit(function(event) {
     event.preventDefault();
-    const roboCount = parseInt($("input#numInput").val());
-    const output = roboResponse(numInput);
+    const roboCount = parseFloat($("input#userNumberInput").val());
+    const output = roboResponse(roboCount);
     $("#output").text(output);
 
   });
-}); 
+});
