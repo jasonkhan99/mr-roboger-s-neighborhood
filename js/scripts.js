c87b77ac03ede2cbd8f1c719c3e58ca25df1a3a2
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-12 09:41:13 -0700

    add function to Business Logic returning false, send to variable in user logic and output it to HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
+// Business Logic
+function roboResponse(numInput) {
+  return false;
+}
+
+
+
+
+
+
+// User Logic
 $(document).ready(function() {
   $("form#formInput").submit(function(event) {
     event.preventDefault();
-    const numInput = parseInt($("input#numInput").val());
-    console.log(numInput);
+    const roboCount = parseInt($("input#numInput").val());
+    const output = roboResponse(numInput);
+    $("#output").text(output);
 
   });
 }); 
